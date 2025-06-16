16535b28599a7b6d16fe1153a3e27c4c5c7c4412
olvera21 <122505771+olvera21@users.noreply.github.com>
2025-06-16 13:12:23 -0600

    Delete IntegraAcademicoPares directory

--- IntegraAcademicoPares/src/Components/atoms/AtomError.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import globalStyles from '../../styles/globalStyles'; // Ajusta la ruta según sea necesario
-
-const AtomError = ({ children, visible }) => (
-  visible ? <Text style={globalStyles.errorText}>{children}</Text> : null
-);
-
-export default AtomError;
--- IntegraAcademicoPares/src/Components/atoms/AtomTitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import globalStyles from '../../styles/globalStyles'; // Ajusta la ruta según sea necesario
-
-const AtomTitle = ({ children, style }) => (
-  <Text style={[globalStyles.title, style]}>{children}</Text>
-);
-
-export default AtomTitle;
